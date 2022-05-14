b9eb5cecbe7a4e12548ac6d736cc847d1e98ac82
Andrei Dobrescu <usingvirtualbox@gmail.com>
2022-05-14 16:54:26 +0300

    ui customisations

--- glowPadExample/src/main/java/com/example/glowpadexample/MainActivity.java
+++ glowPadExample/src/main/java/com/example/glowpadexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.glowpadexample;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.fima.glowpadview.GlowPadView;
 import com.fima.glowpadview.GlowPadView.OnTriggerListener;
@@ -10,6 +11,7 @@ import com.fima.glowpadview.GlowPadView.OnTriggerListener;
 public class MainActivity extends Activity implements OnTriggerListener {
 
 	private GlowPadView glowPadView;
+	private TextView swipeHereTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class MainActivity extends Activity implements OnTriggerListener {
 		setContentView(R.layout.activity_main);
 
 		glowPadView = findViewById(R.id.glow_pad_view);
+		swipeHereTextView = findViewById(R.id.swipe_here_text_view);
 
 		glowPadView.setOnTriggerListener(this);
 
@@ -29,22 +32,21 @@ public class MainActivity extends Activity implements OnTriggerListener {
 
 	@Override
 	public void onGrabbed(View v, int handle) {
-
+		swipeHereTextView.setVisibility(View.GONE);
 	}
 
 	@Override
 	public void onReleased(View v, int handle) {
 		glowPadView.ping();
-
 	}
 
 	@Override
 	public void onTrigger(View v, int target) {
 		final int resId = glowPadView.getResourceIdForTarget(target);
-		if (resId == R.drawable.ic_item_camera) {
-			Toast.makeText(this, "Camera selected", Toast.LENGTH_SHORT).show();
-		} else if (resId == R.drawable.ic_item_google) {
-			Toast.makeText(this, "Google selected", Toast.LENGTH_SHORT).show();
+		if (resId == R.drawable.ic_deny_call) {
+			Toast.makeText(this, "Deny", Toast.LENGTH_SHORT).show();
+		} else if (resId == R.drawable.ic_accept_call) {
+			Toast.makeText(this, "Accept", Toast.LENGTH_SHORT).show();
 		}
 	}
 
